6f31116525eafc4a18664e9243a2c3419bb7d151
osman-gif <osmanngong@gmail.com>
2024-10-11 11:27:46 +0200

    annotated Variables and return value of the 6-sum_mixed_list.py file

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,10 +2,10 @@
 """ function type annotations"""
 
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ type-annotated function sum_list which takes a list input_list of
     floats as argument and returns their sum as a float."""
     return float(sum(mxd_lst))
