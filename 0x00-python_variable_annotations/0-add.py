930dd199861f5aaa5706627639eff097a6c07506
osman-gif <osmanngong@gmail.com>
2024-10-11 10:33:45 +0200

    initial commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,5 @@
+def add(a: float, b: float)-> float:
+    """ type-annotated function add that takes a float a and a float b
+    as arguments and returns their sum as a float."""
+    return float(sum(a+b))
+    
