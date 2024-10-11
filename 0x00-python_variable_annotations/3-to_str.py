9c98a4f3f48869becb9daf6b99a11a40032da65d
osman-gif <osmanngong@gmail.com>
2024-10-11 11:04:58 +0200

    made the 3-to_str.py file compatible with pycodestyle

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -2,7 +2,7 @@
 """ function type annotations"""
 
 
-def to_str(n:float) -> str:
+def to_str(n: float) -> str:
     """ type-annotated function to_str that takes a float n as argument and
     returns the string representation of the float.."""
     return str(n)
