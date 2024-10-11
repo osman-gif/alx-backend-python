89d33c804cd7f7ece8d82d45215e0234277fbdfb
osman-gif <osmanngong@gmail.com>
2024-10-11 10:44:25 +0200

    changed the return value of 0-add.py file from returning sum(a+b) to a+b

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,5 +2,5 @@
 def add(a: float, b: float)-> float:
     """ type-annotated function add that takes a float a and a float b
     as arguments and returns their sum as a float."""
-    return float(sum(a+b))
+    return float(a+b)
     
