e348ddf18afd3d139169dc4b3f33e79f3c440a5c
osman-gif <osmanngong@gmail.com>
2024-10-11 10:58:41 +0200

    documented the module the 2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import math
 """ function type annotations"""
 
 
+import math
 def floor(n: float) -> int:
     """type-annotated function floor which takes a
     float n as argument and returns the floor of the float."""
