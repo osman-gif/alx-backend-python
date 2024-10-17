d69f7a447d91bbfe6692a55dd3c91b222afec869
osman-gif <osmanngong@gmail.com>
2024-10-17 09:05:07 +0200

    Documented the function task_wait_random

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -13,4 +13,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Return a asyncio.Task."""
     return asyncio.Task(wait_random(max_delay))
