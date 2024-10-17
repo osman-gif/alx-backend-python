1fff1e690876ae2d7946018a926745459628a671
osman-gif <osmanngong@gmail.com>
2024-10-17 08:54:08 +0200

    Added the 0x01-python_async_function

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Import wait_random from the previous python file that you’ve
+written and write an async routine called wait_n that takes in 2
+int arguments (in this order): n and max_delay. You will spawn
+wait_random n times with the specified max_delay.
+
+wait_n should return the list of all the delays (float values).
+The list of the delays should be in ascending order without using sort()
+because of concurrency"""
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> list:
+    result: list = []
+    for i in range(n):
+        i = await task_wait_random(max_delay)
+
+        result.append(i)
+    return sorted(result)
