6923ead99cce0b3914110948e9d0e856a29d3f38
osman-gif <osmanngong@gmail.com>
2024-11-03 12:56:52 +0200

    Added the TestMemoize class

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,7 +21,7 @@ nested_map={"a": {"b": 2}}, path=("a", "b")
 import unittest
 from unittest.mock import Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch
 
 
@@ -70,6 +70,29 @@ class TestGetJson(unittest.TestCase):
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
         mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+
+    def test_memoize(self):
+        """Test that memoize caches results correctly."""
+        class MyClass:
+            def __init__(self):
+                self._called = 0
+
+            @memoize
+            def a_method(self):
+                self._called += 1
+                return 42
+
+        my_object = MyClass()
+        result = my_object.a_method()
+        self.assertEqual(result, 42)
+        self.assertEqual(my_object._called, 1)
+
+        result = my_object.a_method()
+        self.assertEqual(result, 42)
+        self.assertEqual(my_object._called, 1)
 
 
 if __name__ == '__main__':
