d0d9604acffe97caba526dbde8db0327d02093f1
osman-gif <osmanngong@gmail.com>
2024-11-03 11:28:09 +0200

    added paramiterized decorator to the test_access_nested_map_exception function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -36,9 +36,12 @@ class TestAccessNestedMap(unittest.TestCase):
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
     def test_access_nested_map_exception(self, nested_map, path):
-        """Test access_nested_map raises KeyError with appropriate message
-        for missing keys."""
+        """raises KeyError with appropriate message for missing keys."""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
 
